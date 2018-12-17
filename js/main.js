781e5ea708f3f5251b1a52c9aaca4c83cd41bff2
Chris Topher <programmingfire@gmail.com>
2018-12-17 16:56:53 -0600

    Build out Card Object

--- js/main.js
+++ js/main.js
@@ -8,13 +8,47 @@ const protocols =
 	{
 		port: "80",
 		service: "http",
+		transportLayerProtocol:"TCP",
 		description: "This service runs on TCP for web servers"
 	},
 	{
+		port: "20/21",
+		service: "FTP",
+		transportLayerProtocol:"TCP",
+		description: "FTP is one of the most commonly used file transfer protocols on the Internet and within private networks. An FTP server can easily be set up with little networking knowledge and provides the ability to easily relocate files from one system to another. FTP control is handled on TCP port 21 and its data transfer can use TCP port 20 as well as dynamic ports depending on the specific configuration."
+	},
+	/*
+	{
 		port: "",
 		service: "",
+		transportLayerProtocol:"",
 		description: ""
 	},
+	*/
 
 
 ]
+
+function protocolObjectSelector(protocolArr)
+{
+	this._protocolArr = protocolArr;
+	this.randomSelect = function()
+	{
+	//	console.log(this._protocolArr[Math.floor(Math.random() * this._protocolArr.length)]);
+		return this._protocolArr[Math.floor(Math.random() * this._protocolArr.length)];
+	}
+}
+
+function Card(cardJSON)
+{
+	this.port = cardJSON.port;
+	this.service = cardJSON.service;
+	this.transportLayerProtocol = cardJSON.transportLayerProtocol;
+	this.description=cardJSON.description;
+
+	this.displayPort = function()
+	{
+		console.log(this.port);
+	}
+	
+}

3694dcfe17b512b8171286e0b9db84a587a7e513
Chris Topher <programmingfire@gmail.com>
2018-12-17 15:50:38 -0600

    Get it up and running

---
+++ js/main.js
@@ -0,0 +1,20 @@
+	function sayHi()
+	{
+		console.log("hi");
+	}
+
+const protocols = 
+[
+	{
+		port: "80",
+		service: "http",
+		description: "This service runs on TCP for web servers"
+	},
+	{
+		port: "",
+		service: "",
+		description: ""
+	},
+
+
+]

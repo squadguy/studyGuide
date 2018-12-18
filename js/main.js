529a85b67891128829b89cce0e3ac5f79c590910
Chris Topher <programmingfire@gmail.com>
2018-12-17 18:18:07 -0600

    Complete submit and and hint functionality

--- js/main.js
+++ js/main.js
@@ -41,11 +41,23 @@ function protocolObjectSelector(protocolArr)
 
 
 //We're going to pretend this is an behavioral interface implementation
+//Come back to this later
 function networkCardBehavior()
 {
+	this.buildHTML = function(appendArea)
+	{
+		const anchor = document.getElementById(appendArea);
+		var node = document.createElement("p");
+		var textnode = document.createTextNode("HELLOWORLD");
+                node.style.background.color="gray";
+                node.appendChild(textnode);
+             //   var x = document.getElementById("machineName");
+                anchor.appendChild(node);  
+
+	}
 	this.displayCard = function(card)
 	{
-		console.log(card.port);
+		console.log(card.port);	
 	}
 }
 
@@ -63,6 +75,63 @@ function Card(cardJSON, networkCardBehavior)
 	}
 
 	this.networkCardBehavior.displayCard(this);
+	this.buildHTML = function()
+	{
+		const somePort = document.getElementById('somePort');
+		somePort.innerHTML = this.port
+
+		const transportLayer = document.getElementById('transportLayerProtocol');
+		transportLayer.innerHTML = this.transportLayerProtocol;
+
+		//Dynamically Build HTML
+		/*
+		const anchor = document.getElementById(appendArea);
+		var divCard = document.createElement("div");
+		divCard.setAttribute("id", "divCard");
+
+		var divCardHead = document.createElement("div");
+		divCardHead.setAttribute("id", "cardHead");
+		var divCardData = document.createElement("div");
+		divCardData.setAttribute("id", "cardData");
+		var h4Port = document.createElement("h4");
+		h4Port.setAttribute("id", "port");
+		var spanSomePort=document.createElement("span");
+		spanSomePort.setAttribute("id","somePort");
+		var textnode = document.createTextNode("HELLOWORLD");
+                spanSomePort.style.background.color="gray";
+                spanSomePort.appendChild(textnode);
+             //   var x = document.getElementById("machineName");
+                anchor.appendChild(spanSomePort);  
+		*/
+
+	}
 	
 	
+}
+
+
+//Make this into a compositie behavior for some object
+function giveHintBehavior(card)
+{
+	let descriptionText = card.description;
+	let appendDescription = document.getElementById('descriptionText');
+	appendDescription.innerHTML = descriptionText;
+
+
+
+}
+
+//Make this into a compositie behavior for some object
+function checkAnswerBehavior(card)
+{
+	let answer = document.getElementById('answer').value;
+
+		if (answer.toLowerCase() === card.service)
+		{
+			console.log("correct");
+		}
+		else
+		{
+			console.log("Wrong");
+		}
 }

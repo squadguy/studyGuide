bd462a26f6cda0b613aba954c9fa2b939200cd9c
Chris Topher <programmingfire@gmail.com>
2019-01-03 15:11:19 -0600

    Create stock data file and ensure it is readable

---
+++ data.js
@@ -0,0 +1,16 @@
+const answers =
+[
+	{
+		question: "What is 100 + 100?",
+		answer: "200"
+	},
+	{
+		question: "What is the third planet from the sun?",
+		answer: "Earth"
+	},
+	{
+		question: "What is man's best friend?",
+		answer: "Dog"
+	}
+
+]
